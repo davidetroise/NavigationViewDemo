7e0ee229af768d22ebf64d64ca0d4cff1f018975
Davide Troise <davide@troise.net>
2022-09-06 18:43:48 +0200

    Initial Commit

---
+++ NavigationViewDemo/NavigationViewDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationViewDemoApp.swift
+//  NavigationViewDemo
+//
+//  Created by Davide Troise with Swift 5.0
+//  for the YouTube channel "Coding con Davide" https://bit.ly/3QJziJE
+//
+
+import SwiftUI
+
+@main
+struct NavigationViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
